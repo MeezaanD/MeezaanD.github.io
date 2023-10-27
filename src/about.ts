2d17383bb0a4816e37cbdb86667c069c64d10c8e
MeezaanD <meezaandavids365@gmail.com>
2023-10-27 09:07:49 +0200

    finished project integration setup

--- src/about.ts
+++ src/about.ts
@@ -2,75 +2,72 @@ type EducationItem = {
 	title: string;
 	description: string;
 	date: string;
-	url: string; // Add a URL property
-  };
-  
-  const education: EducationItem[] = [
+	url: string; 
+};
+
+const education: EducationItem[] = [
 	{
-	  title: 'South Peninsula High School ',
-	  description: 'Graduated and passed Matric with bachelors.',
-	  date: 'January 2017 - December 2021',
-	  url: 'https://sphigh.org/',
+		title: 'South Peninsula High School ',
+		description: 'Graduated and passed Matric with bachelors.',
+		date: 'January 2017 - December 2021',
+		url: 'https://sphigh.org/',
 	},
 	{
-	  title: 'IT Academy ',
-	  description: 'Completed Software Fundamentals Course.',
-	  date: 'March 2022 - March 2023',
-	  url: 'https://itacademy.co.za/',
+		title: 'IT Academy ',
+		description: 'Completed Software Fundamentals Course.',
+		date: 'March 2022 - March 2023',
+		url: 'https://itacademy.co.za/',
 	},
 	{
-	  title: 'Life Choices Academy ',
-	  description: 'Completed a Bootcamp FullStack Developer Coding Course.',
-	  date: 'September 2022 - March 2023',
-	  url: 'https://lifechoicesacademy.com/',
+		title: 'Life Choices Academy ',
+		description: 'Completed a Bootcamp FullStack Developer Coding Course.',
+		date: 'September 2022 - March 2023',
+		url: 'https://lifechoicesacademy.com/',
 	},
 	{
-	  title: 'Life Choices Studio ',
-	  description: 'Assistant Lecturer recruited internally after course at Life Choices Academy.',
-	  date: 'April 2023 - June 2023',
-	  url: 'https://lcstudio.co.za/',
+		title: 'Life Choices Studio ',
+		description: 'Assistant Lecturer recruited internally after course at Life Choices Academy.',
+		date: 'April 2023 - June 2023',
+		url: 'https://lcstudio.co.za/',
 	},
 	{
-	  title: 'IMQS Software ',
-	  description: 'Recruited as an Intern Software Developer.',
-	  date: 'July 2023 - Current',
-	  url: 'https://www.imqs.co.za/',
+		title: 'IMQS Software ',
+		description: 'Recruited as an Intern Software Developer.',
+		date: 'July 2023 - Current',
+		url: 'https://www.imqs.co.za/',
 	},
-  ];
-  
-  const ul = document.querySelector('.my-journey');
-  
-  if (ul) {
+];
+
+const ul = document.querySelector('.my-journey');
+
+if (ul) {
 	education.forEach((item: EducationItem) => {
-	  const li = document.createElement('li');
-	  li.classList.add('education-item'); // Add a class to the <li> element
-  
-	  const a = document.createElement('a');
-	  a.href = item.url;
-	  a.target = "_blank";
-	  a.textContent = item.title; // Set the text content directly
-	  a.classList.add('education-link'); 
-  
-	  const icon = document.createElement('i');
-	  icon.classList.add('fas', 'fa-external-link-alt'); // Add classes for the arrow icon
-  
-	  const pDescription = document.createElement('p');
-	  pDescription.textContent = item.description;
-	  pDescription.classList.add('education-text'); // Add a class to the <p> element
-  
-	  const pDate = document.createElement('p');
-	  pDate.textContent = item.date;
-	  pDate.classList.add('education-date'); // Add a class to the <p> element
-  
-	  a.appendChild(icon); // Append the icon to the <a> element
-  
-	  li.appendChild(a);
-	  li.appendChild(pDescription);
-	  li.appendChild(pDate);
-  
-	  ul.appendChild(li);
+		const li = document.createElement('li');
+		li.classList.add('education-item'); // Add a class to the <li> element
+
+		const a = document.createElement('a');
+		a.href = item.url;
+		a.target = "_blank";
+		a.textContent = item.title; // Set the text content directly
+		a.classList.add('education-link');
+
+		const icon = document.createElement('i');
+		icon.classList.add('fas', 'fa-external-link-alt'); // Add classes for the arrow icon
+
+		const pDescription = document.createElement('p');
+		pDescription.textContent = item.description;
+		pDescription.classList.add('education-text'); // Add a class to the <p> element
+
+		const pDate = document.createElement('p');
+		pDate.textContent = item.date;
+		pDate.classList.add('education-date'); // Add a class to the <p> element
+
+		a.appendChild(icon); // Append the icon to the <a> element
+
+		li.appendChild(a);
+		li.appendChild(pDescription);
+		li.appendChild(pDate);
+
+		ul.appendChild(li);
 	});
-  }
-  
-  
-  
+}
---
+++ src/work.js
@@ -0,0 +1,63 @@
+const projects = [
+    {
+        img: "/assets/ET-preview.png",
+        code: "https://github.com/MeezaanD/MD-Tracker.git",
+        liveLink: "https://my-etracker.netlify.app/",
+        description: "E-Tracker is a versatile mobile web app designed to empower users with efficient and user-friendly budget management tools. With E-Tracker, you can effortlessly create custom budgets tailored to your specific financial goals. The app offers a comprehensive suite of features, including the ability to add, edit, and delete expenses with ease.E-Tracker's intuitive interface ensures that you maintain a clear overview of your finances, as it automatically calculates both the percentage spent and the remaining budget. Whether you're managing personal finances or tracking expenses for a special project, E-Tracker simplifies the budgeting process and helps you stay in control of your money.",
+    },
+];
+const projectContainer = document.querySelector('.project-container');
+if (projectContainer) {
+    projects.forEach((item) => {
+        const row = document.createElement('div');
+        row.classList.add('row');
+        const col1 = document.createElement('div');
+        col1.classList.add('col');
+        const imgLink = document.createElement('a');
+        imgLink.href = item.liveLink;
+        imgLink.target = '_blank';
+        const projectImg = document.createElement('img');
+        projectImg.src = item.img;
+        projectImg.alt = 'project-img';
+        projectImg.classList.add('project-img');
+        imgLink.appendChild(projectImg);
+        col1.appendChild(imgLink);
+        const col2 = document.createElement('div');
+        col2.classList.add('col');
+        const projectTopList = document.createElement('ul');
+        projectTopList.classList.add('project-top-list');
+        const yearLi = document.createElement('li');
+        yearLi.textContent = '2023';
+        const codeLi = document.createElement('li');
+        const codeLink = document.createElement('a');
+        codeLink.href = item.code;
+        codeLink.target = '_blank';
+        codeLink.textContent = 'Code ';
+        codeLink.classList.add('links');
+        const codeIcon = document.createElement('i');
+        codeIcon.classList.add('fab', 'fa-github');
+        codeLink.appendChild(codeIcon);
+        codeLi.appendChild(codeLink);
+        const liveLinkLi = document.createElement('li');
+        const liveLinkA = document.createElement('a');
+        liveLinkA.href = item.liveLink;
+        liveLinkA.target = '_blank';
+        liveLinkA.textContent = 'Visit Site ';
+        liveLinkA.classList.add('links');
+        const liveLinkIcon = document.createElement('i');
+        liveLinkIcon.classList.add('fas', 'fa-external-link-alt');
+        liveLinkA.appendChild(liveLinkIcon);
+        liveLinkLi.appendChild(liveLinkA);
+        projectTopList.appendChild(yearLi);
+        projectTopList.appendChild(codeLi);
+        projectTopList.appendChild(liveLinkLi);
+        const projectText = document.createElement('p');
+        projectText.classList.add('project-text');
+        projectText.textContent = item.description;
+        col2.appendChild(projectTopList);
+        col2.appendChild(projectText);
+        row.appendChild(col1);
+        row.appendChild(col2);
+        projectContainer.appendChild(row);
+    });
+}
