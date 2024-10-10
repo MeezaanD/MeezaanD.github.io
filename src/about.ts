ed92cea9bd6ce487cbeb594706ccb7d37cac7796
MeezaanD <meezaandavids365@gmail.com>
2024-10-10 13:55:31 +0200

    feat: adding new course section

--- src/about.ts
+++ src/about.ts
@@ -2,7 +2,7 @@ type EducationItem = {
 	title: string;
 	description: string;
 	date: string;
-	url: string; 
+	url: string;
 };
 
 const education: EducationItem[] = [
@@ -30,12 +30,6 @@ const education: EducationItem[] = [
 		date: 'July 2023 - July 2024',
 		url: 'https://www.imqs.co.za/',
 	},
-	// {
-	// 	title: 'Silux Control ',
-	// 	description: 'Junior Software Developer.',
-	// 	date: 'August 2024 - Present',
-	// 	url: 'https://www.siluxcontrol.co.uk/',
-	// },
 	{
 		title: 'QTZ Concrete, Essentials Cpt ',
 		description: 'Web Developer / IT team member',
@@ -75,5 +69,85 @@ if (ul) {
 		li.appendChild(pDate);
 
 		ul.appendChild(li);
+	});
+}
+
+type CourseItem = {
+	title: string;
+	institution: string;
+	image: string;  // URL for the course image
+	url: string;
+};
+
+const courses: CourseItem[] = [
+	{
+		title: 'TypeScript: Developer\'s Guide',
+		institution: 'Udemy',
+		image: '/assets/content/typescript.jpg',
+		url: 'https://www.udemy.com/certificate/UC-f6e95682-0010-4526-9199-07bcff8bcb45/?utm_campaign=email&utm_medium=email&utm_source=sendgrid.com',
+	},
+	{
+		title: 'Master Go Programming',
+		institution: 'Udemy',
+		image: '/assets/content/master-go.jpg',
+		url: 'https://www.udemy.com/certificate/UC-32e8a94a-28d1-43f6-a4e0-614b93e88400/?utm_campaign=email&utm_medium=email&utm_source=sendgrid.com',
+	},
+	{
+		title: 'Building Modern Web Applications with Go (Golang)',
+		institution: 'Udemy',
+		image: '/assets/content/web-go.jpg',
+		url: 'https://www.udemy.com/certificate/UC-c069902a-bda0-4269-ab6a-19bc996ab867/?utm_campaign=email&utm_medium=email&utm_source=sendgrid.com',
+	},
+	{
+		title: 'Linux Tutorial',
+		institution: 'Great Learning',
+		image: '/assets/content/linux-tutorial.jpg',
+		url: 'https://olympus.mygreatlearning.com/courses/52823/certificate',
+	},
+	{
+		title: 'Machine Learning Algorithms',
+		institution: 'Great Learning',
+		image: '/assets/content/machine-learn.jpg',
+		url: 'https://olympus.mygreatlearning.com/courses/57914/certificate',
+	},
+	{
+		title: 'Data Science Foundation',
+		institution: 'Great Learning',
+		image: '/assets/content/data-science.jpg',
+		url: 'https://olympus.mygreatlearning.com/courses/13680/certificate',
+	},
+];
+
+const courseList = document.querySelector('.course-list');
+
+if (courseList) {
+	courses.forEach((course: CourseItem) => {
+		const li = document.createElement('li');
+		li.classList.add('course-item'); // Add a class to the <li> element
+
+		const a = document.createElement('a');
+		a.href = course.url;
+		a.target = "_blank";
+		a.classList.add('course-link');
+
+		const img = document.createElement('img');
+		img.src = course.image; // Set the image source
+		img.alt = course.title; // Set alt text
+		img.classList.add('course-image'); // Add a class to the <img> element
+
+		const title = document.createElement('div');
+		title.textContent = course.title;
+		title.classList.add('course-title');
+
+		const institution = document.createElement('div');
+		institution.textContent = course.institution;
+		institution.classList.add('course-institution');
+
+		a.appendChild(img); // Append the image to the <a> element
+		a.appendChild(title); // Append the title to the <a> element
+		a.appendChild(institution); // Append the institution to the <a> element
+
+		li.appendChild(a); // Append the <a> element to the <li> element
+		courseList.appendChild(li); // Append the <li> element to the course list
 	});
 }
