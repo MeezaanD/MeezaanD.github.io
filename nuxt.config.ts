9b382bf3a7e29735e5e229d289845cf4aa4be63d
MeezaanD <meezaandavids365@gmail.com>
2025-11-18 19:49:25 +0200

    fix: deployment issue, resolved components alias

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,4 +1,11 @@
 export default defineNuxtConfig({
   modules: ['@nuxt/ui', '@nuxt/icon'],
   css: ['@/assets/css/tailwind.css'],
+  components: true,
+  alias: {
+    '#components': '/components',
+  },
+  nitro: {
+    preset: 'static',
+  },
 })
