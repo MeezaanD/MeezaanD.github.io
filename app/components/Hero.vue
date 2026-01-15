3c95acee331033e0aee2cfa0c9ced728cd933eb4
MeezaanD <meezaandavids365@gmail.com>
2026-01-15 10:19:39 +0200

    feat: tweaks for icon sizes, added new tech stack

--- app/components/Hero.vue
+++ app/components/Hero.vue
@@ -4,10 +4,10 @@ const techStacks = [
 	{ name: 'Vue', icon: 'logos:vue' },
 	{ name: 'Nuxt', icon: 'logos:nuxt-icon' },
 	{ name: 'Next', icon: 'logos:nextjs-icon' },
+	{ name: 'Astro', icon: 'logos:astro-icon' },
 	{ name: 'Typescript', icon: 'logos:typescript-icon' },
 	{ name: 'Javascript', icon: 'logos:javascript' },
 	{ name: 'Tailwind CSS', icon: 'logos:tailwindcss-icon' },
-	{ name: 'Flutter', icon: 'logos:flutter' },
 	{ name: 'Node.js', icon: 'logos:nodejs-icon' },
 	{ name: 'Python', icon: 'logos:python' },
 	{ name: 'Golang', icon: 'logos:go' },
@@ -31,10 +31,10 @@ const techStacks = [
 
 		<!-- Portrait -->
 		<img src="/portrait.jpg" alt="Meezaan Davids"
-			class="mt-25 w-40 h-40 sm:w-48 sm:h-48 rounded-full object-cover border-4 border-green-500/50 shadow-xl mb-6 animate-fadeIn" />
+			class="mt-25 w-30 h-30 sm:w-36 sm:h-36 rounded-full object-cover border-4 border-green-500/50 shadow-xl mb-6 animate-fadeIn" />
 
 		<!-- Headline -->
-		<h1 class="text-2xl sm:text-5xl md:text-5xl font-extrabold mb-4 text-center animate-fadeIn">
+		<h1 class="text-2xl sm:text-4xl md:text-4xl font-extrabold mb-4 text-center animate-fadeIn">
 			<span class="text-white-500">Hi, I’m </span>
 			<span class="text-green-500">Meezaan Davids</span>
 		</h1>
@@ -51,7 +51,7 @@ const techStacks = [
 			<div v-for="tech in techStacks" :key="tech.name"
 				class="flex flex-col items-center transition duration-300 hover:-translate-y-1 hover:scale-105">
 				<UIcon :name="tech.icon"
-					class="w-12 sm:w-16 h-12 sm:h-10 text-green-500/90 hover:text-green-400 transition-colors" />
+					class="w-12 sm:w-12 h-8 sm:h-8 text-green-500/90 hover:text-green-400 transition-colors" />
 				<span class="mt-2 text-xs sm:text-sm text-gray-400 font-medium text-center">
 					{{ tech.name }}
 				</span>
@@ -73,10 +73,17 @@ const techStacks = [
 				<span>GitHub</span>
 			</a>
 
-			<a href="https://www.linkedin.com/in/meezaan-davids-4a7aa8265/" target="_blank"
+			<a href="https://www.linkedin.com/in/meezaan-davids/" target="_blank"
 				class="flex items-center gap-2 px-4 py-2 bg-white/10 backdrop-blur-md rounded-lg border border-white/20 hover:bg-green-400 transition-colors">
 				<UIcon name="logos:linkedin-icon" class="w-5 h-5 sm:w-6 sm:h-6" />
 				<span>LinkedIn</span>
+			</a>
+
+			<!-- Certifications -->
+			<a href="https://meezaand.github.io/certifications/" target="_blank"
+				class="flex items-center gap-2 px-4 py-2 bg-white/10 backdrop-blur-md rounded-lg border border-white/20 hover:bg-green-400 transition-colors">
+				<UIcon name="mdi:certificate-outline" class="w-5 h-5 sm:w-6 sm:h-6" />
+				<span>Certifications</span>
 			</a>
 		</section>
 	</section>
